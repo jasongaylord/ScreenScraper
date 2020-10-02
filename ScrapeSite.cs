87ad7a3a6724a2c0be2245f9c315a4a3f56240f5
Jason Gaylord <jason.gaylord@biberk.com>
2020-10-01 22:40:37 -0400

    Clean-up the ScrapeSite function.

--- ScrapeSite.cs
+++ ScrapeSite.cs
@@ -1,15 +1,22 @@
-using System;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using HtmlAgilityPack;
 
 namespace JasonGaylord.Functions
 {
+    public sealed class ProductAvailability
+    {
+        public bool IsAvailable { get; set; }
+        public string Url { get; set; }
+    }
+
     public static class ScrapeSite
     {
         [FunctionName("ScrapeSite")]
@@ -19,17 +26,29 @@ namespace JasonGaylord.Functions
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
+            var products = new List<ProductAvailability>();
+            products.Add(await Scraper.GetProductAvailability("https://www.walmart.com/ip/Xbox-Series-X/443574645"));
+            products.Add(await Scraper.GetProductAvailability("https://www.walmart.com/ip/Microsoft-Xbox-One-S-1TB-All-Digital-Edition-Console-Disc-free-Gaming-White-NJP-00024/560014078"));
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            return new OkObjectResult(products);
+        }
+    }
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+    public static class Scraper
+    {
+        public static async Task<ProductAvailability> GetProductAvailability(string uri)
+        {
+            var productAvailability = new ProductAvailability(){ Url = uri };
 
-            return new OkObjectResult(responseMessage);
+            var web = new HtmlWeb();
+            var html = await web.LoadFromWebAsync(productAvailability.Url);
+            var htmlString = html.DocumentNode;
+            var buttons = htmlString.SelectNodes("//button").ToList();
+
+            var outOfStock = buttons.Count(c => c.InnerHtml.Contains("Get in-stock alert"));
+            productAvailability.IsAvailable = outOfStock == 0;
+
+            return productAvailability;
         }
     }
 }
